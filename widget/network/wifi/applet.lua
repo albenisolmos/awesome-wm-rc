f622fcfc2d8dcb70141f838a9633d32a2c253efd
albenisolmos <albenisolmos88@gmail.com>
2021-09-29 20:27:11 -0400

    Remove unused variable

--- widget/network/wifi/applet.lua
+++ widget/network/wifi/applet.lua
@@ -5,8 +5,6 @@ local app          = require 'apps'
 local widget_large = require 'widget.network.wifi.large'
 local build_applet = require 'widget.applet'
 
-local status = 'Wireless network is desconnected'
-
 local applet = build_applet(
 	wibox.widget.imagebox(beautiful.icon_wifi_off),
 	widget_large,
@@ -16,10 +14,8 @@ local applet = build_applet(
 awesome.connect_signal('wifi::update', function(essid)
 	if essid then
 		applet:set_image(beautiful.icon_wifi_on)
-		status = 'Connected to ' .. essid
 	else
 		applet:set_image(beautiful.icon_wifi_off)
-		status = 'Wireless network is desconnected'
 	end
 end)
 
