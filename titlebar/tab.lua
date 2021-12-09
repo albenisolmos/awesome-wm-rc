2226ff56a746704997248ae81e677d736eb57398
albenisolmos <albenisolmos88@gmail.com>
2021-12-09 17:40:25 -0400

    Fix multiples bugs

--- titlebar/tab.lua
+++ titlebar/tab.lua
@@ -1,10 +1,9 @@
 local awful = require('awful')
 local wibox = require('wibox')
 local keygrabber = require('awful.keygrabber')
+local clickable = require('widget.clickable')
 
 local Tab = {}
-local all_witabs = {}
-local all_current_clients = {}
 
 function awful.client.object.set_witab(cli, witab)
 	cli._witab = witab 
@@ -15,20 +14,38 @@ function awful.client.object.get_witab(self)
 	return self._witab
 end
 
-local function tab_widget_new(c)
-	return wibox.widget {
-		layout = wibox.layout.align.horizontal,
-		awful.titlebar.widget.iconwidget(c),
-		awful.titlebar.widget.titlewidget(c),
-		nil
+local function tab_widget_new(c, witab)
+	local widget = wibox.widget {
+		widget = clickable,
+		{
+			layout = wibox.layout.align.horizontal,
+			awful.titlebar.widget.iconwidget(c),
+			awful.titlebar.widget.titlewidget(c),
+			nil
+		}
 	}
+
+	widget:buttons({
+		awful.button({}, 1, function()
+			witab:focus_client(c)
+		end)
+	})
+
+	return widget
 end
 
 local function get_spawn_cmd(cli)
 	return string.lower(cli.class)
 end
 
+local function find_index(tbl, target)
+	for i, el in pairs(tbl) do
+		if el == target then return i end
+	end
+end
+
 local function syncronize(cli, witab)
+	awful.titlebar.hide(cli)
 	cli:connect_signal('property::geometry', function(c)
 		local geo = c:geometry()
 		witab:geometry({
@@ -38,26 +55,67 @@ local function syncronize(cli, witab)
 			height = 35
 		})
 	end)
+
+	cli:connect_signal('unmanage', function(c)
+		tab_close(c, true)
+	end)
+end
+
+local function tab_close(cli, no_kill_cli)
+	local index = find_index(cli.witab.clients, cli)
+	cli.witab.widget:remove(index)
+
+	if not no_kill_cli then
+		cli:kill()
+	end
 end
 
 local function tab_new(cli, is_first_tab)
 	cli = cli or client.focus
 	local witab = cli.witab
-	witab.focused_client = cli
-
-	awful.titlebar.hide(cli)
-	syncronize(cli, witab)
-
-	if not is_first_tab then
-		awful.spawn(get_spawn_cmd(cli), {callback = function(c) cli = c end})
+	local function on_new()
+		syncronize(cli, witab)
 		table.insert(witab.clients, cli)
+		_G.print('ONNEW')
+		witab:focus_client(cli)
+		witab.widget:add(tab_widget_new(cli, witab))
 	end
 
-	witab.widget:add(tab_widget_new(cli))
+	if not is_first_tab then
+		awful.spawn(
+			get_spawn_cmd(cli),
+			{ callback = function(c)
+				cli = c
+				cli.witab = witab
+				on_new()
+			end }
+		)
+	else
+		on_new()
+	end
+end
+
+local function for_each(tbl, step)
+	if not tbl or not step then return end
+	for i, el in pairs(tbl) do
+		step(el, i)
+	end
+end
+
+local function tab_focus(target)
+	if type(target) == 'number' then
+		local curr_cli = client.focus
+		local index = find_index(curr_cli.witab.clients, curr_cli)
+		local cli = curr_cli.witab.clients[target]
+
+		if index ~= target and cli then
+			curr_cli.witab:focus_client(cli)
+		end
+	end
 end
 
 function Tab.enable_tabs(cli)
-	if not cli then return error ('No client provided')end
+	if not cli then return error('No client provided') end
 
 	local container = wibox.layout.fixed.horizontal()
 	local witab = wibox {
@@ -70,12 +128,39 @@ function Tab.enable_tabs(cli)
 		bg = '#212121',
 		widget = container,
 	}
-	witab.clients = {cli}
+	witab.clients = {}
 	witab.focused_client = nil
 
+	function witab:focus_client(c)
+		if self.focused_client == c then
+			return
+		elseif self.focused_client then
+			self.focused_client.hidden = true
+		end
+
+		c.hidden = false
+		self.focused_client = c
+	end
+
 	cli.witab = witab
-	table.insert(all_witabs, witab)
 	tab_new(cli, true)
+
+	keygrabber {
+		export_keybinding = true,
+		stop_key = 'Mod4',
+		stop_event = 'release',
+		start_callback = function()
+		end,
+		stop_callback = function()
+		end,
+		root_keybindings = {
+			awful.key {
+				modifiers = {'Mod4', 'Mod1'},
+				keygroup = 'numrow',
+				on_press = tab_focus
+			}
+		}
+	}
 end
 
 function Tab.init()
