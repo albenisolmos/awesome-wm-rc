86a00cb3ca6eee4a8b57af2f33eeabb373132c88
albenisolmos <albenisolmos88@gmail.com>
2022-03-11 23:58:36 -0400

    Reimplement the hot-corner module

--- display/hot-corners.lua
+++ display/hot-corners.lua
@@ -1,12 +1,85 @@
 local wibox     = require('wibox')
 local gtimer     = require('gears.timer')
+local placement = require('awful.placement')
 local beautiful = require('beautiful')
 local dpi       = beautiful.xresources.apply_dpi
 
-local execute_time = 0.40
+local hotedge = {mt = {}}
 
+function hotedge.new(args)
+	local screen = args.screen
+	local props = {
+		callback = true,
+		timeout = 0.5
+	}
+	local edge = wibox {
+		screen    = screen,
+		visible   = true,
+		ontop     = true,
+		opacity   = 0.0,
+		x         = screen.geometry.x,
+		y         = screen.geometry.y,
+		type      = 'utility',
+	}
+
+	if args.position then
+		if args.position == 'bottom' or args.position == 'top' then
+			edge.height = dpi(3);
+			edge.width = screen.geometry.width
+		elseif args.position == 'left' or args.position == 'right' then
+			edge.height = screen.geometry.height
+			edge.width = dpi(3)
+		end
+
+		local place = placement[args.position] + placement.center
+		place(edge)
+	end
+
+	for key, value in pairs(args) do
+		if props[key] then
+			props[key] = value
+		else
+			edge[key] = value
+		end
+	end
+
+	local timer = gtimer {
+		timeout   = props.timeout,
+		call_now  = false,
+		autostart = false,
+		callback  = function(self)
+			if props.callback then
+				props.callback(self)
+			end
+			self:stop()
+		end
+	}
+
+	edge:connect_signal('mouse::enter', function()
+		if timer.started then
+			timer:again()
+		else
+			timer:start()
+		end
+	end)
+
+	edge:connect_signal('mouse::leave', function()
+		if timer.started then
+			timer:stop()
+		end
+	end)
+
+	return edge
+end
+
+function hotedge.mt.__call(_, ...)
+	return hotedge.new(...)
+end
+
+return setmetatable(hotedge, hotedge.mt)
+--[[
 return function(screen)
-	screen.corner_bc = wibox {
+	screen.edge_bc = wibox {
 		screen    = screen,
 		visible   = true,
 		ontop     = true,
@@ -28,7 +101,7 @@ return function(screen)
 		end
 	}
 
-	screen.corner_bc:connect_signal('mouse::enter', function()
+	screen.edge_bc:connect_signal('mouse::enter', function()
 		if bc_timer.started then
 			bc_timer:again()
 		else
@@ -36,13 +109,13 @@ return function(screen)
 		end
 	end)
 
-	screen.corner_bc:connect_signal('mouse::leave', function()
+	screen.edge_bc:connect_signal('mouse::leave', function()
 		if bc_timer.started then
 			bc_timer:stop()
 		end
 	end)
 
-	screen.corner_bt = wibox {
+	screen.edge_bt = wibox {
 		screen    = screen,
 		visible   = true,
 		ontop     = true,
@@ -64,7 +137,7 @@ return function(screen)
 		end
 	}
 
-	screen.corner_bt:connect_signal('mouse::enter', function()
+	screen.edge_bt:connect_signal('mouse::enter', function()
 		if bt_timer.started then
 			bt_timer:again()
 		else
@@ -72,9 +145,9 @@ return function(screen)
 		end
 	end)
 
-	screen.corner_bt:connect_signal('mouse::leave', function()
+	screen.edge_bt:connect_signal('mouse::leave', function()
 		if bt_timer.started then
 			bt_timer:stop()
 		end
 	end)
-end
+end]]
