d5aec58622cb110dcb1367b8420250a363dbe080
albenisolmos <albenisolmos88@gmail.com>
2022-03-12 00:00:33 -0400

    Delete deprecate module

--- widget/center/applet.lua
+++
@@ -1,8 +0,0 @@
-local wibox        = require('wibox')
-local beautiful    = require('beautiful')
-local applet = require 'widget.applet'
-
-return applet(
-	wibox.widget.imagebox(beautiful.icon_center),
-	require 'widget.center'
-)
