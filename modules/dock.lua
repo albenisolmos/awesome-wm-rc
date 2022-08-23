0e1bd3712256d9bcc8abd2165fdb460659c7ebf5
albenisolmos <albenisolmos88@gmail.com>
2022-08-23 12:26:48 -0400

    refactory code

--- modules/dock.lua
+++ modules/dock.lua
@@ -1,17 +1,54 @@
 local placement = require('awful.placement')
-local ascreen = require('awful.screen')
 local spawn = require('awful.spawn')
 local timer = require('gears.timer')
 local beautiful = require("beautiful")
 local dpi = beautiful.xresources.apply_dpi
 
-local animate = require 'utils.animate'
+local animate = require('utils.animate')
+local uclient = require('utils.client')
 local partial_show
-local dock = {}
+local timer_callback
 
-local function current_clients()
-	local t = ascreen.focused().selected_tag
-	return t:clients()
+local function need_hide_dock()
+	local clients = uclient.get_clients()
+
+	if #clients > 0 then
+		for _, client in pairs(clients) do
+			if (client.maximized or client.fullscreen) and not client.minimized then
+				return true
+			end
+		end
+	end
+
+	return false
+end
+
+local time_hide_dock = timer {
+	timeout = 1,
+	autostart = false,
+	single_shot = true,
+	callback = function ()
+		timer_callback()
+	end
+}
+
+local function dock_show(dock, coord)
+	if dock.y == coord then return end
+	partial_show = true
+	animate.move.y(dock, coord)
+	if SETTINGS.dock_autohide then
+		dock.ontop = true
+		time_hide_dock:start()
+	else
+		dock:struts({ bottom = dpi(48) })
+	end
+end
+
+local function dock_hide(dock, coord)
+	if dock.y == coord then return end
+	partial_show = false
+	dock:struts({ bottom = 0 })
+	animate.move.y(dock, coord)
 end
 
 return { init = function()
@@ -20,40 +57,15 @@ return { init = function()
 		skip_taskbar = true,
 		sticky = true,
 		callback = function(cli)
-			dock = cli
+			local dock = cli
 			local s = cli.screen
-			local normal_coord = s.geometry.height - dpi(52)
+			local dock_height = dock.height
+			local normal_coord = s.geometry.height - dock_height - dpi(9)
 			local offscreen_coord = s.geometry.height + 1
 
-			local function dock_hide()
-				if dock.y == offscreen_coord then return end
-				partial_show = false
-				dock:struts({ bottom = 0 })
-				animate.move.y(dock, offscreen_coord)
+			timer_callback = function()
+				dock_hide(dock, offscreen_coord)
 			end
-
-			local function need_hide_dock()
-				local clients = current_clients()
-
-				if #clients > 0 then
-					for _, client in pairs(clients) do
-						if (client.maximized or client.fullscreen) and not client.minimized then
-							return true
-						end
-					end
-				end
-
-				return false
-			end
-
-			local time_hide_dock = timer {
-				timeout = 1,
-				autostart = false,
-				single_shot = true,
-				callback = function()
-					dock_hide()
-				end
-			}
 
 			dock:connect_signal('mouse::leave', function()
 				if partial_show then
@@ -67,18 +79,6 @@ return { init = function()
 				end
 			end)
 
-			local function dock_show()
-				if dock.y == normal_coord then return end
-				partial_show = true
-				animate.move.y(dock, normal_coord)
-				if SETTINGS.dock_autohide then
-					dock.ontop = true
-					time_hide_dock:start()
-				else
-					dock:struts({ bottom = dpi(48) })
-				end
-			end
-
 			awesome.connect_signal('dock::kill', function ()
 				dock:kill()
 				dock = nil
@@ -87,14 +87,14 @@ return { init = function()
 			awesome.connect_signal('dock::update', function()
 				if not dock then return end
 				if need_hide_dock() then
-					dock_hide()
+					dock_hide(dock, offscreen_coord)
 				else
-					dock_show()
+					dock_show(dock, normal_coord)
 				end
 			end)
 
 			awesome.connect_signal('dock::partial_show', function()
-				if dock and dock.y == normal_coord then return end
+				if not dock or dock.y == normal_coord then return end
 				dock.ontop = true
 				partial_show = true
 				animate.move.y(dock, normal_coord)
@@ -104,13 +104,13 @@ return { init = function()
 			local function client_toggle_dock(c)
 				if c.maximized or c.fullscreen then
 					dock.ontop = true
-					dock_hide()
+					dock_hide(dock, offscreen_coord)
 				else
 					if need_hide_dock() then
 						return
 					else
 						dock.ontop = false
-						dock_show()
+						dock_show(dock, normal_coord)
 					end
 				end
 			end
@@ -122,7 +122,7 @@ return { init = function()
 				for _, c in pairs(clients) do
 					if not c.floating then
 						dock.ontop = true
-						dock_hide()
+						dock_hide(dock, offscreen_coord)
 						return
 					end
 				end
