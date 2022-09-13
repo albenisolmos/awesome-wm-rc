006b0687d892039afb82ea242691c6020bd614a7
albenisolmos <albenisolmos88@gmail.com>
2022-09-12 22:27:36 -0400

    Restructure theme folder

--- test.lua
+++
@@ -1,9 +0,0 @@
-local tbl1 = {loco1={1,2,3}, loco2={4,5,6}}
-local tbl2 = {["loco1"]={1,2,3}, ["loco2"]={4,5,6}}
-local tbl3 = {{1,2,3}, {4,5,6}}
-
-for n, tlayouts in pairs(tbl1) do
-	print(n, tlayouts)
-end
-
-print(select('#', {1,2,3}, {123,1}))
